aa0d970149d0d88266cc9301f2993abe4350c022
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-12-13 19:36:07 +0600

    side navabr -style

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../Sidebar/Sidebar";
+import "./layout.scss";
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar></Sidebar>
+    </div>
+  );
+};
+
+export default Layout;
